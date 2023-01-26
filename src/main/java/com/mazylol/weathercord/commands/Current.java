0b0ee9d11426ddb44a91fabb56934930b1d85da3
Mazylol <landonp95@gmail.com>
2023-01-26 10:35:23 -0500

    Less stupid formatting

--- src/main/java/com/mazylol/weathercord/commands/Current.java
+++ src/main/java/com/mazylol/weathercord/commands/Current.java
@@ -18,7 +18,7 @@ public class Current extends ListenerAdapter {
             String location = Objects.requireNonNull(event.getOption("location")).getAsString().replaceAll("\\s", "");
             String unit = "metric";
 
-            if (event.getOption("units") != null ) {
+            if (event.getOption("units") != null) {
                 unit = Objects.requireNonNull(event.getOption("units")).getAsString();
             }
 
@@ -31,20 +31,18 @@ public class Current extends ListenerAdapter {
                     default -> " C";
                 };
 
-                EmbedBuilder eb = new EmbedBuilder();
-
-                eb.setThumbnail("https://openweathermap.org/img/w/" + weather.weather.get(0).icon + ".png");
-                eb.setColor(0x00AE86);
-                eb.setTimestamp(Instant.now());
-
-                eb.setTitle("Current Weather for " + weather.name);
-                eb.setDescription(weather.weather.get(0).description);
-                eb.addField("Temperature", weather.main.temp + abbv, true);
-                eb.addField("High", weather.main.temp_max + abbv, true);
-                eb.addField("Low", weather.main.temp_min + abbv, true);
-                eb.addField("Humidity", weather.main.humidity + "%", true);
-                eb.addField("Pressure", weather.main.pressure + " hPa", true);
-                eb.addField("Wind Speed", weather.wind.speed + " m/s", true);
+                EmbedBuilder eb = new EmbedBuilder()
+                        .setTitle("Current Weather for " + weather.name)
+                        .setDescription(weather.weather.get(0).description)
+                        .setThumbnail("https://openweathermap.org/img/w/" + weather.weather.get(0).icon + ".png")
+                        .addField("Temperature", weather.main.temp + abbv, true)
+                        .addField("High", weather.main.temp_max + abbv, true)
+                        .addField("Low", weather.main.temp_min + abbv, true)
+                        .addField("Humidity", weather.main.humidity + "%", true)
+                        .addField("Pressure", weather.main.pressure + " hPa", true)
+                        .addField("Wind Speed", weather.wind.speed + " m/s", true)
+                        .setColor(0x00AE86)
+                        .setTimestamp(Instant.now());
 
                 event.replyEmbeds(eb.build()).setEphemeral(true).queue();
             } catch (IOException | InterruptedException e) {
